9d7bdeee38960b24190842b8cd558f0dab2efef7
SUMALATHA-AVULA <sumalathaavula9573@gmail.com>
2021-08-03 19:17:56 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('SUMALATHA-AVULA/docker-nodejs-demo')
+            repositoryName('example/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
